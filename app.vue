5be61ec5b27564360be6e7fd60361334d25bbff0
Harshna N Haswani <hharshna.n@gmail.com>
2022-11-24 22:32:53 +0530

    Initial commit

---
+++ app.vue
@@ -0,0 +1,43 @@
+<template>
+  <Form @submit="onSubmit" :validation-schema="schema">
+    <label for="email">Your Email</label>
+    <Field id="email_addr" name="email_addr" type="email" />
+    <ErrorMessage name="email_addr" />
+
+    <label for="password">Your Password</label>
+    <Field id="password" name="acc_pazzword" type="password" />
+    <ErrorMessage name="acc_pazzword" />
+
+    <button>Submit</button>
+  </Form>
+</template>
+
+<script setup lang="ts">
+import { Field, Form, ErrorMessage } from 'vee-validate';
+import * as Yup from 'yup';
+
+const schema = Yup.object().shape({
+  email_addr: Yup.string().email().required().label('Email Address'),
+  acc_pazzword: Yup.string().min(5).required().label('Your Password'),
+});
+
+function onSubmit(values) {
+  alert(JSON.stringify(values, null, 2));
+}
+</script>
+
+<style>
+input,
+label {
+  display: block;
+  margin-top: 20px;
+}
+
+input + span {
+  display: block;
+}
+
+button {
+  margin-top: 20px;
+}
+</style>
---
+++ nuxt.config.ts
@@ -0,0 +1,5 @@
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default {
+  telemetry: false,
+  buildModules: ["@nuxt/typescript-build"]
+};
